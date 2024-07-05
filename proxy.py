273db61eb9490f2a73d044cc18b1cf7198738a78
Carlo <284474102@qq.com>
2024-07-05 18:04:36 +0800

    test

--- proxy.py
+++ proxy.py
@@ -1,4 +1,6 @@
 import requests
+from urllib.request import getproxies
+print(getproxies())
 # 现在，所有通过requests发出的请求都将自动使用代理
 response = requests.get(
     'https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/LICENSE.md?download=true')
