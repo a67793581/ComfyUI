70ba1f1d2a3f39afbc9e8f0ef684fb017fa2a36f
Carlo <284474102@qq.com>
2024-08-14 13:27:02 +0800

    "download.py中添加了加载模型配置和生成GenerationConfig的部分，并在下载tokenizer时添加了clean_up_tokenization_spaces参数"

--- download.py
+++ download.py
@@ -1,14 +1,27 @@
 import argparse
-from transformers import AutoModel, AutoTokenizer
+
+from transformers import AutoModel, AutoConfig, AutoTokenizer, GenerationConfig
 
 
 def main(model_name, local_dir):
+    # 加载模型配置
+    # model_config = AutoConfig.from_pretrained(model_name)
+    # non_default_generation_parameters = {}
+    # for parameter_name, default_value in model_config._get_generation_defaults().items():
+    #     if hasattr(model_config, parameter_name) and getattr(model_config, parameter_name) != default_value:
+    #         non_default_generation_parameters[parameter_name] = getattr(model_config, parameter_name)
+    #         delattr(model_config, parameter_name)
+    #
+    # # 创建 GenerationConfig
+    # if non_default_generation_parameters:
+    #     gen_config = GenerationConfig(**non_default_generation_parameters)
+    #     gen_config.save_pretrained(local_dir)
+
     # 下载模型
     model = AutoModel.from_pretrained(model_name)
     model.save_pretrained(local_dir)
-
     # 下载 tokenizer
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    tokenizer = AutoTokenizer.from_pretrained(model_name, clean_up_tokenization_spaces=True)
     tokenizer.save_pretrained(local_dir)
 
 
